41a09dccd060869220d30261653aa47dd53d6f9d
GrazZzys <nikita2859.gylin.01@gmail.com>
2021-10-13 14:16:09 +0500

    add .env file

--- config/db.php
+++ config/db.php
@@ -1,9 +1,12 @@
 <?php
 
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=mysql;port=3306;dbname=news',
-    'username' => 'root',
-    'password' => 'root',
+    'username' => $username,
+    'password' => $password,
     'charset' => 'utf8',
 ];
